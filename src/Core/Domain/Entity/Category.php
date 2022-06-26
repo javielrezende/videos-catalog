5e73c794808cef2fc24520f80f0f84fd1a8b1d0b
Roger Rezende <javielrezende@gmail.com>
2022-06-25 21:55:42 -0300

    Validate the entity category

--- src/Core/Domain/Entity/Category.php
+++ src/Core/Domain/Entity/Category.php
@@ -3,6 +3,7 @@
 namespace Core\Domain\Entity;
 
 use Core\Domain\Entity\Traits\MagicMethodsTrait;
+use Core\Domain\Exception\EntityValidationException;
 
 class Category {
     use MagicMethodsTrait;
@@ -12,7 +13,9 @@ class Category {
         protected string $name = '',
         protected string $description = '',
         protected bool $isActive = true,
-    ) {}
+    ) {
+        $this->validate();
+    }
 
     public function activate(): void
     {
@@ -26,7 +29,23 @@ class Category {
 
     public function update(string $name = null, string $description = null): void
     {
+        $this->validate();
         $this->name = $name ?? $this->name;
         $this->description = $description ?? $this->description;
+    }
+
+    public function validate()
+    {
+        if (empty($this->name)) {
+            throw new EntityValidationException('Name can not be empty');
+        }
+
+        if (strlen($this->name) < 3  || strlen($this->name) > 255) {
+            throw new EntityValidationException('Name is invalid');
+        }
+
+        if ($this->description !== '' && (strlen($this->description) < 3  || strlen($this->description) > 255)) {
+            throw new EntityValidationException('Description is invalid');
+        }
     }
 }
--- tests/Unit/Domain/Entity/CategoryUnitTest.php
+++ tests/Unit/Domain/Entity/CategoryUnitTest.php
@@ -3,15 +3,16 @@
 namespace Tests\Unit\Domain\Entity;
 
 use Core\Domain\Entity\Category;
+use Core\Domain\Exception\EntityValidationException;
 use PHPUnit\Framework\TestCase;
+use Throwable;
 
 class CategoryUnitTest extends TestCase {
     public function testGetAttributes()
     {
         $category = new Category(
             name: 'Category 1',
-            description: 'Category 1 description',
-            isActive: true
+            description: 'Category 1 description'
         );
 
         $this->assertEquals('Category 1', $category->name);
@@ -82,5 +83,19 @@ class CategoryUnitTest extends TestCase {
 
         $this->assertEquals('New Category 4', $category->name);
         $this->assertEquals('New Category 4.1 description', $category->description);
+    }
+
+    public function testThrowExcepetionWhenUpdateName()
+    {
+        try {
+            $category = new Category(
+                name: 'Ca',
+                description: 'Category 5 description'
+            );
+
+            $this->assertTrue(false);
+        } catch (Throwable $th) {
+            $this->assertInstanceOf(EntityValidationException::class, $th);
+        }
     }
 }
