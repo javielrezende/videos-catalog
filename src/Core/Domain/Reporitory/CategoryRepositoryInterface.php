cd8839b3c7f7d0d16e05806dc49a615382dfbd62
Roger Rezende <javielrezende@gmail.com>
2022-07-07 22:57:18 -0300

    Add paginated categories

--- src/Core/Domain/Reporitory/CategoryRepositoryInterface.php
+++ src/Core/Domain/Reporitory/CategoryRepositoryInterface.php
@@ -7,8 +7,8 @@ use Core\Domain\Entity\Category;
 interface CategoryRepositoryInterface {
     public function insert(Category $category): Category;
     public function findById(string $id): Category;
-    public function findAll(string $filter = '', string $order = 'DESC'): PaginationInterface;
-    public function paginate(string $filter = '', string $order = 'DESC', int $page = 1, int $totalPerPage = 15): array;
+    public function findAll(string $filter = '', string $order = 'DESC'): array;
+    public function paginate(string $filter = '', string $order = 'DESC', int $page = 1, int $totalPerPage = 15): PaginationInterface;
     public function update(Category $category): Category;
     public function delete(string $id): bool;
     public function toCategory(object $data): Category;
